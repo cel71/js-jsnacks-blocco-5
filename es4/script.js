d081092da83d90ea25da920c4555c5823f5f64c2
cel71 <celestialc71@libero.it>
2021-06-16 15:13:28 +0200

    js-jsnacks-blocco-5 es2

--- es4/script.js
+++ es4/script.js
@@ -1,4 +1,57 @@
 
+// Snack 4 -
+// Creare un array di 3 studenti (3 oggetti in un array, quindi) con queste proprietà:
+// nome
+// cognome
+// age
+// descrizione
+// Per nome, cognome ed age assegnate direttamente nel codice i valori che volete.
+// ATTENZIONE: la descrizione invece sarà l’unico valore vuoto, stringa vuota.
+// {
+//   nome: ‘Pippo’,
+//   cognome: ‘Baudo’,
+//   age: 80,
+//   descrizione: ‘’
+// } 
+// Successivamente, con dei prompt chiediamo di volta in volta all’utente la descrizione di ciascun studente, salviamola quindi all’interno della sua proprietà.
+// Nota bene: questo naturalmente, in un ciclo, per ciascun studente. ;)
+// Stampiamo con un console.log il risultato di questo array di studenti aggiornato.
+// Bonus
+// Visualizzare nell’html questi dati in una forma tabellare.
+
+var studenti = [
+    {
+        nome: "Mario",
+        cognome: "Rossi",
+        age: 23,
+        descrizione: "",
+    },
+    {
+        nome: "Francesca",
+        cognome: "Verdi",
+        age: 20,
+        descrizione: "",
+    },
+    {
+        nome: "Ferdinando",
+        cognome: "Bianchi",
+        age: 25,
+        descrizione: "",
+    },
+];
+
+for (var x = 0; x < 3; x++) {
+    var user = prompt("Inserisci una descrizione dell' alunno");
+    studenti[x].descrizione = user;
+}
+
+console.log(studenti);
+
+for (var i = 0; i < studenti.length; i++) {
+    for (var key in studenti[i]) {
+        document.getElementById("studenti").innerHTML += "<li>" + key + ": " + studenti[i] [key] + "</li>";
+    }
+}
 
 
 
